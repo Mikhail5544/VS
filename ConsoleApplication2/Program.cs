0358227837f608c7d94408d465a02c153cd5f85b
Mikhail Chernykh <mikhail5544@gmail.com>
2017-06-16 17:56:28 +0300

    First GitHub commit!

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace GitHubTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Git Hub!");
         }
     }
 }
