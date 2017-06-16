af25b13df2a783cb4d7a025da4b4220a39021063
Mikhail Chernykh <mikhail5544@gmail.com>
2017-06-16 18:11:19 +0300

    Second commit

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git Hub!");
+            Console.WriteLine("Second commit!");
         }
     }
 }
